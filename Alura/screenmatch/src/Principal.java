12e80fff7861a8926ae90d11708e7bbcbe2c5885
Lucas <pv.lucassilva@gmail.com>
2025-05-16 09:25:57 -0300

    inheritance and polymorphism

--- Alura/screenmatch/src/Principal.java
+++ Alura/screenmatch/src/Principal.java
@@ -1,3 +1,5 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
 import br.com.alura.screenmatch.modelos.*;
 
 public class Principal {
@@ -6,6 +8,7 @@ public class Principal {
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -14,14 +17,37 @@ public class Principal {
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadoras(10);
+        lost.setEpsodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar 2");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+
+
         System.out.println("\n-----------------------------------");
 
         Pessoa pessoa = new Pessoa();
         pessoa.saudacao();
-
-        Calculadora calculadora = new Calculadora();
-        int resultado = calculadora.dobrarNumero(5);
-        System.out.println(resultado);
 
         System.out.println("\n-----------------------------------");
 
---
+++ Alura/screenmatch/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,45 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo {
+    private int temporadoras;
+    private boolean ativa;
+    private int epsodiosPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getTemporadoras() {
+        return temporadoras;
+    }
+
+    public void setTemporadoras(int temporadoras) {
+        this.temporadoras = temporadoras;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpsodiosPorTemporada() {
+        return epsodiosPorTemporada;
+    }
+
+    public void setEpsodiosPorTemporada(int epsodiosPorTemporada) {
+        this.epsodiosPorTemporada = epsodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadoras * epsodiosPorTemporada * minutosPorEpisodio;
+    }
+}

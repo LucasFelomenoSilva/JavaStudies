04883de12eacb71117663b9eedaa192c173271ba
Lucas <pv.lucassilva@gmail.com>
2025-05-23 11:52:31 -0300

    add devdojo

---
+++ DevDojo/CalculadorDiferencaIdade.java
@@ -0,0 +1,15 @@
+/*
+Crie uma classe chamada CalculadorDiferencaIdade
+Crie uma variável para guardar a primeira idade
+Crie uma variável para guardar a segunda idade
+Crie uma variável para guardar a diferença entre as idades
+*/
+
+public class CalculadorDiferencaIdade {
+	public static void main(String[] args){
+		int idade1 = 30;
+		int idade2 = 20;
+		int diferencaIdade = idade1 - idade2;
+		System.out.println("A diferenca entre as duas idades eh: " + diferencaIdade);
+	}
+}
---
+++ DevDojo/Main.java
@@ -0,0 +1,10 @@
+// Comentário
+
+public class Main {
+	public static void main(String[] args) {
+		int idade = 35;
+		System.out.println("idade = " + idade);
+	}
+}
+
+

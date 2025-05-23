ce7c45e4dcabf31c6a151b81cc538d1af98c24a3
Lucas <pv.lucassilva@gmail.com>
2025-05-23 16:18:56 -0300

    CalculatorImpost add

---
+++ DevDojo/logicaProgramacao/src/CalculatorImpost.java
@@ -0,0 +1,8 @@
+public class CalculatorImpost {
+    public static void main(String[] args) {
+        float salary = 2500.00F;
+        float descont = 10;
+        float percentageOfSalary = salary * (descont / 100);
+        System.out.println("O desconto no salário é de: " + percentageOfSalary);
+    }
+}

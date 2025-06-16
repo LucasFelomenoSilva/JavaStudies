029ff2eb1cec50fe56d18cba32049e58054d334c
Lucas <pv.lucassilva@gmail.com>
2025-06-16 10:00:28 -0300

    maratona-java 06

---
+++ maratona-java/src/ademy/devdojo/maratonajava/introducao/Aula06EstruturaDeRepeticao04.java
@@ -0,0 +1,15 @@
+package ademy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturaDeRepeticao04 {
+    public static void main(String[] args) {
+        double preco = 30000;
+
+        for (int parcela = 1; parcela <= preco ; parcela++) {
+            double valorParcela = preco / parcela;
+            if (valorParcela < 1000){
+                break;
+            }
+            System.out.println("Parcela " + parcela + " R$" + valorParcela);
+        }
+    }
+}

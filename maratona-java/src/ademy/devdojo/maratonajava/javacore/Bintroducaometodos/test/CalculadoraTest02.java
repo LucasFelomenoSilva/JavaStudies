bc98d90e268c6d0b29527c862fbb927b6defd564
Lucas <pv.lucassilva@gmail.com>
2025-06-20 10:17:03 -0300

    maratona-java 08

---
+++ maratona-java/src/ademy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package ademy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import ademy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(5, 5);
+    }
+}

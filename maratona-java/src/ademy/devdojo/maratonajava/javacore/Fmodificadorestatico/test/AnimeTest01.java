bee61751ba2b1bb7e77e33a1d060cc0a1373781c
Lucas <pv.lucassilva@gmail.com>
2025-06-26 11:32:50 -0300

    maratona-java 12

---
+++ maratona-java/src/ademy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package ademy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import ademy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}

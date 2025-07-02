87ea3ce2046cc1c9aac117a311624d58fd710433
Lucas <pv.lucassilva@gmail.com>
2025-07-02 13:01:57 -0300

    maratona-java 14

---
+++ maratona-java/src/ademy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -0,0 +1,18 @@
+package ademy.devdojo.maratonajava.javacore.Hheranca.dominio;
+
+public class Funcionario extends Pessoa {
+    private double salario;
+
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
